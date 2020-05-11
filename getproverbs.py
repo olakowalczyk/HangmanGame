b4f6f12bc85c79d0905271fbdd9d3e02e80c171f
olakowalczyk <60587401+olakowalczyk@users.noreply.github.com>
2020-05-11 13:17:17 +0200

    PEP8

--- getproverbs.py
+++ getproverbs.py
@@ -1,24 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 
-def get_proverbs(): # gets proverbs from website
+
+def get_proverbs():  # gets proverbs from website
     url = "https://www.phrases.org.uk/meanings/proverbs.html"
     res = requests.get(url)
     html_page = res.content
 
     soup = BeautifulSoup(html_page, 'html.parser')
-    elements = soup.findAll('p', attrs={'class' : 'phrase-list'}) 
+    elements = soup.findAll('p', attrs={'class': 'phrase-list'})
 
-    proverb_list=[]
+    proverb_list = []
     proverb_list = extract_proverbs(proverb_list, elements)
     return proverb_list
-    
-def extract_proverbs(proverb_list, elements): # eliminates proverbs with some annotation etc.
+
+
+# eliminates proverbs with some annotation etc.
+def extract_proverbs(proverb_list, elements):
     for element in elements:
         element.getText()
         proverb_list.append(element.getText())
 
-    subs = [" - ",";","]",".","?", "nowt"] # 
+    subs = [" - ", ";", "]", ".", "?", "nowt"]
     for i in proverb_list[:]:
         for sub in subs[:]:
             if sub in i:
--- hangman.py
+++ hangman.py
@@ -3,73 +3,80 @@ from getproverbs import get_proverbs
 from colorama import init
 init(autoreset=True)
 
-sentences= get_proverbs()
-#sentences=['abc']
-hangman ={1: '', 2: '/', 3: '/\\', 4: '|\n|\n|\n/\\', 5: '____\n|  \n|\n|\n/\\', 6: '____\n|  |\n|\n|\n/\\', 7: '____\n|  |\n|  o\n|\n/\\', 8: '____\n|  |\n|  o\n|  |\n/\\', 9: '____\n|  |\n|  o\n| /|\n/\\', 10: '____\n|  |\n|  o\n| /|\\\n/\\', 11: '____\n|  |\n|  o\n| /|\\\n/\\/', 12: '____\n|  |\n|  o\n| /|\\\n/\\/ \\'}
+sentences = get_proverbs()
+hangman = {1: '', 2: '/', 3: '/\\', 4: '|\n|\n|\n/\\', 5: '____\n|  \n|\n|\n/\\', 6: '____\n|  |\n|\n|\n/\\', 7: '____\n|  |\n|  o\n|\n/\\', 8: '____\n|  |\n|  o\n|  |\n/\\',
+           9: '____\n|  |\n|  o\n| /|\n/\\', 10: '____\n|  |\n|  o\n| /|\\\n/\\', 11: '____\n|  |\n|  o\n| /|\\\n/\\/', 12: '____\n|  |\n|  o\n| /|\\\n/\\/ \\'}
+
 
 def main():
-    sentenceToGuess = sentence() # get random sentence
-    sentenceToGuessMask = mask_sentence(sentenceToGuess) # hides the sentence to the player
+    sentenceToGuess = sentence()  # get random sentence
+    # hides the sentence to the player
+    sentenceToGuessMask = mask_sentence(sentenceToGuess)
     print("\nGuess the proverb: " + sentenceToGuessMask + "\nYou have 11 chances")
     chance = 1
-    
-    while chance <= 11: # check the letter till chances are available
+
+    while chance <= 11:  # check the letter till chances are available
         print("\n===== Chance " + str(chance) + " =====")
         letter = provide_value("\nProvide one letter: ")
-        sentenceToGuessMask = replace_with_letter(letter, sentenceToGuess, sentenceToGuessMask)
-        sentenceToGuessMask =''.join(sentenceToGuessMask)
+        sentenceToGuessMask = replace_with_letter(
+            letter, sentenceToGuess, sentenceToGuessMask)
+        sentenceToGuessMask = ''.join(sentenceToGuessMask)
         print("\n" + str(sentenceToGuessMask) + "\n")
-        
-        if letter in sentenceToGuess and len(letter)==1: # chance do not increment where letter match
+
+        # chance do not increment where letter match
+        if letter in sentenceToGuess and len(letter) == 1:
             chance = chance
         else:
-            chance = chance+1 
-        print('\033[33m' + hangman[chance]) # print hangman appropiate to chance
-        
+            chance = chance + 1
+        # print hangman appropiate to chance
+        print('\033[33m' + hangman[chance])
+
         if str(sentenceToGuessMask) == sentenceToGuess:
-            print('\033[32m'+"\nYOU WIN :)")
+            print('\033[32m' + "\nYOU WIN :)")
             print("\nPlay again? (y)")
-            key=input()
-            if key=='y':
+            key = input()
+            if key == 'y':
                 main()
             break
-             
+
     else:
-        print('\033[31m'+"\nGAME OVER :( \nProverb: " + str(sentenceToGuess))        
+        print('\033[31m' + "\nGAME OVER :( \nProverb: " + str(sentenceToGuess))
         print("\nPlay again? (y)")
-        key=input()
-        if key=='y':
+        key = input()
+        if key == 'y':
             main()
-            
-def replace_with_letter(letter, sentence, sentenceMask): #replace mask ____ with letter when letter match sentence letter
+
+
+# replace mask "____" with letter when letter match sentence letter
+def replace_with_letter(letter, sentence, sentenceMask):
     sentence = list(sentence)
     sentenceMask = list(sentenceMask)
-    index_l=-1
-    
-    for l in sentence:
-        index_l=index_l + 1
-        if l == letter:
-            sentenceMask[index_l]=letter
-        if l == letter.capitalize():
-           sentenceMask[index_l]=letter.capitalize()
-    return sentenceMask
-          
+    index_l = -1
 
-def provide_value(desc): #provides letter
+    for l in sentence:
+        index_l = index_l + 1
+        if l == letter:
+            sentenceMask[index_l] = letter
+        if l == letter.capitalize():
+            sentenceMask[index_l] = letter.capitalize()
+    return sentenceMask
+
+
+def provide_value(desc):  # provides letter
     value = input(desc)
     return value
 
-def sentence(): # draws sentence
+
+def sentence():  # draws sentence
     sentence = random.choice(sentences)
     return sentence
 
-def mask_sentence(sentence): # provides masked sentence
+
+def mask_sentence(sentence):  # provides masked sentence
     for l in sentence:
         if l != ' ' and l != "’" and l != ',' and l != '\'':
             sentence = sentence.replace(l, '_')
     return sentence
-    
+
 
 main()
-
-
